3ae907380006cce9bff1968e11a732e1a8989456
duggiralasravan <duggirala.sravan@gmail.com>
2025-06-30 00:06:58 +0530

    Changing Dynamic return type to generic
    Changing Dynamic return type to generic

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from .schemas import SearchQuery, EmployeeResponse
 from .crud import search_employees
 from .rate_limiter import RateLimiter
+from typing import List,Dict,Any
 
 app = FastAPI()
 rate_limiter = RateLimiter(max_requests=5, per_seconds=60)
@@ -14,6 +15,6 @@ async def rate_limit(request: Request, call_next):
         return JSONResponse(status_code=429, content={"detail": "Rate limit exceeded"})
     return await call_next(request)
 
-@app.get("/employees/search", response_model=list[EmployeeResponse])
+@app.get("/employees/search", response_model=list[Dict[str,Any]])
 def search_employees_api(params: SearchQuery = Depends()):
     return search_employees(params)
